07a72567735ef91d00cb92c7d6abe8bd52fed49f
studyingheesu <heesu.naver@gmail.com>
2022-01-05 17:46:44 +0900

    router

--- src/server.js
+++ src/server.js
@@ -3,18 +3,28 @@ import express from 'express';
 const app = express();
 const PORT = 4000;
 
-const testMiddleware = (req, res, next) => {
-  console.log(`hello, ${req.url}`);
-  if (req.url === '/') {
-    res.send('<h1>Welcome</h1>');
-  }
+const logerMiddleware = (req, res, next) => {
+  console.log(req.path);
   next();
 };
-const welcomeMiddleware = (req, res) => {
-  console.log('you should not be here');
-  res.send('Hoho');
-};
-app.use(testMiddleware, welcomeMiddleware);
+app.use(logerMiddleware);
+
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send('home');
+globalRouter.get('/', handleHome);
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send('edit user');
+userRouter.get('/edit', handleEditUser);
+
+const videoRouter = express.Router();
+const handlewWatchVideo = (req, res) => res.send('watch video');
+videoRouter.get('/watch', handlewWatchVideo);
+
+app.use('/', globalRouter);
+app.use('/users', userRouter);
+app.use('/videos', videoRouter);
+
 app.get('/', () => console.log('Somebody is trying to go home.'));
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
