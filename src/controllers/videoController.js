571e9c0cb8399d6bc8086a8af01d2d6a781f92a7
studyingheesu <heesu.naver@gmail.com>
2022-01-07 22:32:53 +0900

    template variables

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,14 @@
-export const trending = (req, res) => res.render('home');
-export const see = (req, res) => res.render('watch');
-export const edit = (req, res) => res.render('edit');
+export const trending = (req, res) =>
+  res.render('home', {
+    pageTitle: 'Home',
+  });
+export const see = (req, res) =>
+  res.render('watch', {
+    pageTitle: 'Watch',
+  });
+export const edit = (req, res) =>
+  res.render('edit', {
+    pageTitle: 'Edit',
+  });
 export const upload = (req, res) => res.send('Edit');
 export const remove = (req, res) => res.send('Edit');
