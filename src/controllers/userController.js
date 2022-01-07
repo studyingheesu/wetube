2e0cac7e6ca37a2015f981ba56fc427dd272c6ab
studyingheesu <heesu.naver@gmail.com>
2022-01-07 22:07:52 +0900

    request params

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,7 @@
 export const join = (req, res) => res.send('Join');
 export const edit = (req, res) => res.send('Edit User');
 export const remove = (req, res) => res.send('Remove User');
+export const login = (req, res) => res.send('Login User');
+export const search = (req, res) => res.send('Search User');
+export const logout = (req, res) => res.send('Logout User');
+export const see = (req, res) => res.send('See User');
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,5 @@
 export const trending = (req, res) => res.send('Home Page Videos');
-export const watch = (req, res) => res.send('Watch');
+export const see = (req, res) => res.send('Watch');
 export const edit = (req, res) => res.send('Edit');
+export const upload = (req, res) => res.send('Edit');
+export const remove = (req, res) => res.send('Edit');
