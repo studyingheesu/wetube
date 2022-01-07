d1a9ec92d8ddcfecc5dc8eddb13f14149e408ce3
studyingheesu <heesu.naver@gmail.com>
2022-01-07 22:27:24 +0900

    template, block

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,5 @@
 export const trending = (req, res) => res.render('home');
 export const see = (req, res) => res.render('watch');
-export const edit = (req, res) => res.send('Edit');
+export const edit = (req, res) => res.render('edit');
 export const upload = (req, res) => res.send('Edit');
 export const remove = (req, res) => res.send('Edit');
